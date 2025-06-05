fb304aeabdd601fd104f091d2096c100a9c8fa0a
aruwer <amandamaffini@gmail.com>
2025-06-05 20:17:00 -0300

    nivel1

---
+++ src/Nivel1.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Nivel1 {
+    public Nivel1(){}
+        public static void main(String[] args) {
+            ArrayList<Perguntas> perguntas = Arquivo.carregarPerguntas("C:\\Users\\1292510067\\IdeaProjects\\BugzippyA3\\src\\perguntas.txt");
+
+            ArrayList<Perguntas> perguntasNivel1 = Filtragem.sortearPerguntasPorNivel(perguntas, 1);
+
+            for (Perguntas p : perguntasNivel1) {
+                System.out.println(p.getEnunciado());
+                System.out.println(p.getAlternativa1());
+                System.out.println(p.getAlternativa2());
+                System.out.println(p.getAlternativa3());
+                System.out.println(p.getAlternativa4());
+                System.out.println();
+            }
+        }
+}
