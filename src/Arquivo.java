9038cb63e655814427643fa33a494b0b430e9bea
aruwer <amandamaffini@gmail.com>
2025-06-05 20:03:02 -0300

    arquivo atualizado

--- src/Arquivo.java
+++ src/Arquivo.java
@@ -2,23 +2,21 @@ import java.io.*;
 import java.util.*;
 public class Arquivo {
 
-        public static ArrayList<Pergunta> carregarPerguntas(String arq) {
-            ArrayList<Pergunta> perguntas = new ArrayList<>();
+        public static ArrayList<Perguntas> carregarPerguntas(String arq) {
+            ArrayList<Perguntas> perguntas = new ArrayList<>();
             try (BufferedReader br = new BufferedReader(new FileReader(arq))) {
                 String linha;
                 while ((linha = br.readLine()) != null) {
                     String[] partes = linha.split(";");
-                    if (partes.length == 6) {
+                    if (partes.length == 7) {
                         int nivel = Integer.parseInt(partes[0]);
-                        Pergunta p = new Pergunta(nivel, partes[1], partes[2], partes[3], partes[4], partes[5]);
+                        Perguntas p = new Perguntas(nivel, partes[1], partes[2], partes[3], partes[4], partes[5], partes[6]);
                         perguntas.add(p);
                     }
                 }
-
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
             return perguntas;
         }
     }
